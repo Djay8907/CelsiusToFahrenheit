ec511be50975fb3a1c670560918ed542aa4bdbc8
Derek <djsmith8907@gmail.com>
2020-04-13 18:49:22 -0400

    Initial Commit

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+
+/**
+ * @author Student
+ *
+ */
+public class CelsiusToFahrenheit {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Celsius 10 is Fahrenheit Degree");
+        System.out.println((9.0 / 5) * 10 + 32);
+        // TODO Auto-generated method stub
+
+    }
+
+}
+
